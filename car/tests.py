8533ddfd4cd6b1a6484fb39fc177ae69e03859c6
Hamim Al Mahdi Russell <russell310@gmail.com>
2024-06-02 19:43:57 +0800

    added model test

--- car/tests.py
+++ car/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .factories import CarFactory
 
-# Create your tests here.
+
+class CarModelTest(TestCase):
+    """
+    Test case for the Car model.
+
+    This class contains tests to verify the functionality of the Car model
+    """
+
+    def setUp(self):
+        self.car = CarFactory()
+
+    def test_car_str(self):
+        self.assertEqual(
+            str(self.car),
+            f"{self.car.color} Car, L: {self.car.length}, W: {self.car.weight}, V: {self.car.velocity}",
+        )
+
